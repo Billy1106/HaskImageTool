0d2dcf1018a3a9bbd7c3ce907f65dcebabce1688
Billy <ryotabilly@gmail.com>
2024-02-23 20:18:01 -0800

    use GaussianOperation

--- app/ProcessResize.hs
+++ app/ProcessResize.hs
@@ -1,41 +1,49 @@
 module ProcessResize (processResize) where
 
 import Codec.Picture
-import Data.List (isPrefixOf)
-
+import FindArgValue (findArgValue)
+import GaussianOperation (applyGaussian)
 
 parseDimensions :: [String] -> (Int, Int)
-parseDimensions args =
-    let width = read $ drop 8 $ head [arg | arg <- args, "--width=" `isPrefixOf` arg]
-        height = read $ drop 9 $ head [arg | arg <- args, "--height=" `isPrefixOf` arg]
-    in (width, height)
+parseDimensions args = (width, height)
+  where
+    -- use read to convert the string to an integer
+    width = read $ findArgValue "--width=" args
+    height = read $ findArgValue "--height=" args
 
 parseIOPaths :: [String] -> (FilePath, FilePath)
-parseIOPaths args =
-    let inputPath = head [drop 8 arg | arg <- args, "--input=" `isPrefixOf` arg]
-        outputPath = head [drop 9 arg | arg <- args, "--output=" `isPrefixOf` arg]
-    in (inputPath, outputPath)
+parseIOPaths args = (inputPath, outputPath)
+    where
+        inputPath = findArgValue "--input=" args
+        outputPath = findArgValue "--output=" args
 
-
+-- Resizing uses "takes nth pixel" strategy.
 resizeImage :: DynamicImage -> Int -> Int -> Image PixelRGB8
-resizeImage dynImg newWidth newHeight =
-    let img = convertRGB8 dynImg
-        (width, height) = (imageWidth img, imageHeight img)
-        scaleX = fromIntegral width / fromIntegral newWidth
-        scaleY = fromIntegral height / fromIntegral newHeight
-        getPixel x y = pixelAt img (floor $ fromIntegral x * scaleX) (floor $ fromIntegral y * scaleY)
-    in generateImage getPixel newWidth newHeight
-
+resizeImage resizingImage newWidth newHeight = generateImage getPixel newWidth newHeight
+  where
+    img = convertRGB8 resizingImage
+    scaleX = fromIntegral (imageWidth img) / fromIntegral newWidth
+    scaleY = fromIntegral (imageHeight img) / fromIntegral newHeight
+    
+    getPixel x y = 
+        let originalX = floor $ fromIntegral x * scaleX
+            originalY = floor $ fromIntegral y * scaleY
+        in applyGaussian img originalX originalY
 
 processResize :: [String] -> IO ()
 processResize args = do
-    let (width, height) = parseDimensions args
-        (inputPath, outputPath) = parseIOPaths args
     eitherImg <- readImage inputPath
     putStrLn $ "Resizing image: " ++ inputPath
-    case eitherImg of
-        Left err -> putStrLn $ "Error reading image: " ++ err
-        Right dynImg -> do
-            let resizedImg = resizeImage dynImg width height
-            savePngImage outputPath (ImageRGB8 resizedImg)
-            putStrLn $ "Image resized and saved to " ++ outputPath
+    -- If readImage returns a Left err, handleError is called with err as its argument. 
+    -- If readImage returns a Right resizingImage, processImage is called with resizingImage as its argument.
+    either handleError processImage eitherImg
+        where
+            -- Destructure the tuples directly in the function 
+            (inputPath, outputPath) = parseIOPaths args
+            (width, height) = parseDimensions args
+
+            handleError err = putStrLn $ "Error reading image: " ++ err
+
+            processImage resizingImage = do
+                savePngImage outputPath . ImageRGB8 $ resizeImage resizingImage width height
+                putStrLn $ "Image resized and saved to " ++ outputPath
